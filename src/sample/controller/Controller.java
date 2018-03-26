b371bfc32ac8e61f27232beb84c275fe0debde25
kamciokodzi <kamil.kolodziej@student.put.poznan.pl>
2018-03-26 15:02:13 +0200

    Add filechooser

--- src/sample/controller/Controller.java
+++ src/sample/controller/Controller.java
@@ -2,19 +2,21 @@ package sample.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.highgui.Highgui;
 import sample.util.SinogramUtils;
 
-import java.io.ByteArrayInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import javax.swing.plaf.FileChooserUI;
+import java.io.*;
 
 public class Controller {
 
@@ -28,19 +30,19 @@ public class Controller {
     public Pane originalPane;
     public Pane sinogramPane;
     public Pane reconstructedPane;
+    private String filePath;
+
 
 
     @FXML
     void initialize() throws FileNotFoundException {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         scrollBarsSetUp();
-        settingPane();
-
     }
 
 
     private void settingPane() throws FileNotFoundException{
-        Image image = new Image(new FileInputStream("Shepp_logan.png"), 400, 400, true, false );
+        Image image = new Image(new FileInputStream(filePath), 400, 400, true, false );
 
         originalPane.setBackground(new Background(new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT)));
@@ -49,16 +51,26 @@ public class Controller {
     public void execute(ActionEvent actionEvent) {
         // =======SINOGRAM====
         MatOfByte bytes = new MatOfByte();
-        Mat sinogram_mat = SinogramUtils.createSinogram("Shepp_logan.png", (int)stepScrollBar.getValue(), (int)angleScrollBar.getValue(), (int)detectorScrollBar.getValue());
+        Mat sinogram_mat = SinogramUtils.createSinogram(filePath, (int)stepScrollBar.getValue(), (int)angleScrollBar.getValue(), (int)detectorScrollBar.getValue());
         Highgui.imencode(".bmp", sinogram_mat, bytes);
         Image sin = new Image(new ByteArrayInputStream(bytes.toArray()), 400, 400, true, false);
         sinogramPane.setBackground(new Background(new BackgroundImage(sin, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
                 BackgroundSize.DEFAULT)));
     }
 
-    public void chooseFile(ActionEvent actionEvent) {
-    }
+    public void chooseFile(ActionEvent actionEvent) throws IOException{
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
 
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image");
+        File file = fileChooser.showOpenDialog(stage);
+
+        if(file!=null){
+            filePath = file.getAbsolutePath();
+            fileLabel.setText("Opened file: " + file.getName());
+            settingPane();
+        }
+    }
 
     private void scrollBarsSetUp(){
         stepScrollBar.setMin(0);
